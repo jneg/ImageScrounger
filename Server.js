de7b656c34b3aa05f9115c56a3362d49b4ba47f2
javon <jnegahba@calpoly.edu>
2017-03-24 21:10:04 -0700

    Implemented scrounge feature populating image grid

--- Server.js
+++ Server.js
@@ -1,11 +1,35 @@
 const Express = require('express')
 const Server = Express()
+const HttpServer = require('http').createServer(Server)
+const Io = require('socket.io')(HttpServer)
+const request = require('request')
+const cheerio = require('cheerio')
 
 const port = 3000
+
+Io.on('connection', function (socket) {
+  console.log('[' + socket.id + ']' + ' Connected')
+  socket.on('scroungeUri', function (uri) {
+    console.log('[' + socket.id + ']' + ' Received scroungeUri \"' + uri + "\"")
+    var imageUris = []
+    request(uri, function (error, response, body) {
+      $ = cheerio.load(body)
+      $('body').find('img').each(function (i, v) {
+        var imageUri = $(v).attr('src')
+        if (!new RegExp('^(?:[a-z]+:)?//', 'i').test(imageUri)) {
+          imageUri = uri + '/' + imageUri
+        }
+        imageUris.push(imageUri)
+      })
+      console.log(imageUris)
+      socket.emit('imageUris', imageUris)
+    })
+  })
+})
 
 Server.use('/Vue', Express.static('Vue'))
 Server.get('/', (req, res) => {
   res.sendFile(__dirname + '/Html/ImageScrounger.html')
 })
 
-Server.listen(port, () => console.log('ImageScrounger listening on port ' + port))
+HttpServer.listen(port, () => console.log('ImageScrounger listening on port ' + port))
--- Vue/ImageScrounger.js
+++ Vue/ImageScrounger.js
@@ -1,11 +1,81 @@
+var socket = io('http://localhost:3000')
+
+Vue.component('ImageScroungerURIBar', {
+  'template':
+   '<div class="ui basic segment">\
+      <div class="ui center aligned grid">\
+        <div class="ten wide column">\
+          <div class="ui fluid action input">\
+            <input v-model="inputUri" placeholder="URI">\
+            <button @click="scroungeUri" class="ui teal button">Scrounge</button>\
+          </div>\
+        </div>\
+      </div>\
+    </div>',
+  'props': ['uriLength'],
+  'data': function () {
+    return {
+      'inputUri': ''
+    }
+  },
+  'methods': {
+    'scroungeUri': function () {
+      socket.emit('scroungeUri', this.inputUri)
+    }
+  }
+})
+
+Vue.component('ImageScroungerImageGrid', {
+  'template':
+   '<div class="ui basic segment">\
+      <div v-for="uri in uris" :style="imgContainerStyle">\
+        <a :href="uri" target="_blank">\
+          <img :src="uri" :style="imgStyle"/>\
+        </a>\
+      </div>\
+    </div>',
+  'props': ['uris', 'imageSize'],
+  'computed': {
+    'imgContainerStyle': function () {
+      return {
+        display: 'inline',
+        margin: '5px'
+      }
+    },
+    'imgStyle': function () {
+      return {
+        maxHeight: this.imageSize + 'px',
+        maxWidth: this.imageSize + 'px'
+      }
+    }
+  }
+})
+
 Vue.component('ImageScroungerController', {
   'template':
    '<main>\
-     ImageScroungerController\
-   </main>'
+     <ImageScroungerURIBar></ImageScroungerURIBar>\
+     <ImageScroungerImageGrid :uris="uris" :imageSize="imageSize"></ImageScroungerImageGrid>\
+    </main>',
+  'data': function () {
+    return {
+      'uris': ['https://static.pexels.com/photos/56875/tree-dawn-nature-bucovina-56875.jpeg',
+               'https://cdn.pixabay.com/photo/2014/12/22/00/07/tree-576847_1280.png',
+               'http://weknowyourdreams.com/images/tree/tree-01.jpg',
+               'https://lh3.googleusercontent.com/-XOLl50tjLug/V05rl_eU98I/AAAAAAABNtQ/3WhFFDlmFCk/orr-park-tree-carvings-37.jpg?imgmax=1600',
+               'https://treesatlanta.org/wp-content/files_mf/cache/th_515b92c7a4ae9e77c6b55fb35c882ce8_homesliderimage5.jpg',
+               'http://treegroup.info/tree-april-19.gif'],
+      'imageSize': 250
+    }
+  },
+  'mounted': function () {
+    socket.on('imageUris', function (uris) {
+      this.uris = uris
+    }.bind(this))
+  }
 })
 
 new Vue({
   'el': '#Mount',
-  'template': '<ImageScroungerController/>'
+  'template': '<ImageScroungerController></ImageScroungerController>'
 })
